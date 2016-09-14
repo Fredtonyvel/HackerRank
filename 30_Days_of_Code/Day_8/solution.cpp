7f901c1a9618395844942b3b98cd75cb02db640f
Fredtonyvel <vfredanthony@yahoo.com>
2016-09-14 19:20:26 -0400

    Added Day_8 solution

---
+++ 30_Days_of_Code/Day_8/solution.cpp
@@ -0,0 +1,57 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include <map>
+#include <string>
+using namespace std;
+
+int main() 
+{
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
+
+	int n;
+	string name, query_name;
+	int phone;
+
+	cin >> n;	//readin integer for creating test phone_numbers cases
+
+	//declared a map object with "string" key type and "int" value
+	map<string, int> phone_number;
+
+	for (int i = 0; i < n; i++)		
+	{
+		cin >> name >> phone;	//readin key and value
+		
+		//Method #1: Creating key/value pair
+		phone_number[name] = phone;
+
+		//Method #2: Insert the key/value pair
+		//phone_number.insert(pair<string,int>(name, phone));
+	}
+
+	cout << endl;
+
+	while (cin >> query_name)
+	{
+		if (phone_number.find(query_name) != phone_number.end())
+		{
+			cout << query_name << "=" << phone_number.find(query_name)->second << endl;
+		}
+		else
+			cout << "Not Found" << endl;
+	}
+
+	cout << endl;
+
+	map<string, int>::iterator it;
+
+	//Print contents of the map
+	for (it = phone_number.begin(); it != phone_number.end(); it++) 
+	{
+		cout << it->first << "=" << it->second << endl;
+	}
+
+    return 0;
+}
